201e624b9bb77c9588cadf545c7c9627ce904b15
Daniar Mukhametshin <jishikukuto@gmail.com>
2022-04-22 22:10:21 +0300

    22/04

--- src/components/Asside/Asside.jsx
+++ src/components/Asside/Asside.jsx
@@ -1,2 +1,36 @@
 import React from "react";
 import './asside.scss'
+
+export const Asside = () => {
+  return (
+    <asside className="asside">
+      <div className="container">
+        <div className="asside__wrapper">
+        </div>
+      </div>
+
+      <div>
+        <a href="/">Меню</a>
+
+        <a href="/">Главная</a>
+        <a href="/">О нас</a>
+        <a href="/">Контакты</a>
+
+        <a href="/">Категории</a>
+
+        <a href="/">Кухни</a>
+        <a href="/">Спальни</a>
+        <a href="/">Гостинные</a>
+        <a href="/">Прихожие</a>
+        <a href="/">Офисная мебель</a>
+        <a href="/">Детская</a>
+        <a href="/">Акции</a>
+        <a href="/">Новинки</a>
+        <a href="/">Матрасы</a>
+        <a href="/">Мягкая мебель</a>
+        <a href="/">Шкафы</a>
+      </div>
+
+    </asside>
+  )
+}
